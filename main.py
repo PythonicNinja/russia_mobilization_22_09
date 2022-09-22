ecdd38f948aca9cf499880bdf8d67105a544ce74
Wojtek Nowak <mail@pythonic.ninja>
2022-09-22 17:21:07 +0200

    added pandas analysis of age

---
+++ main.py
@@ -0,0 +1,29 @@
+import datetime as dt
+import pandas as pd
+from pathlib import Path
+
+source_file = Path(__file__).parent / 'Priziv1volna.txt'
+
+
+def get_source_df():
+    df = pd.read_csv(
+        source_file,
+        delimiter="	",
+        names=[
+            "name",
+            "birthday",
+            "address",
+            "phone",
+            "region",
+        ],
+    )
+    df['birthday'] = pd.to_datetime(df['birthday'])
+    df['age'] = (dt.datetime.today() - df['birthday']).dt.days / 365.25
+    df['age'] = df['age'].astype(int)
+    return df
+
+
+df = get_source_df()
+
+age_distribution = df.age.value_counts()
+print(f"age_distribution {age_distribution}")
